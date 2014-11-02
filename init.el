b009f208353e3985996ed60a1e2b18b8834d69c2
Andrew Mcveigh <me@andrewmcveigh.com>
2014-11-02 12:53:30 +0100

    Use global company mode

--- init.el
+++ init.el
@@ -50,6 +50,8 @@
 (require 'neotree)
 (require 'fill-column-indicator)
 
+(add-hook 'after-init-hook 'global-company-mode)
+
 ;; Use auto-complete as completion at point
 (defun set-auto-complete-as-completion-at-point-function ()
   (setq completion-at-point-functions '(auto-complete)))
@@ -86,9 +88,6 @@
 
 ;; eldoc in clojure
 (add-hook 'cider-interaction-mode-hook 'cider-turn-on-eldoc-mode)
-
-(add-hook 'cider-repl-mode-hook 'company-mode)
-(add-hook 'cider-mode-hook 'company-mode)
 
 ;; Cider... don't annoy me
 (setq cider-hide-special-buffers t)
